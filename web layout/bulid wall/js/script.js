e151a2ac5d0ec9bd1eecc6881c0b964f031634be
sakshi2310 <cdmisakshi@gmail.com>
2024-03-09 08:05:34 +0530

    learning and projects

---
+++ web layout/bulid wall/js/script.js
@@ -0,0 +1,36 @@
+$('#slider').owlCarousel({
+     loop:true,
+     margin:10,
+     nav:true,
+     dots:false,
+     responsive:{
+         0:{
+             items:1
+         },
+         600:{
+             items:3
+         },
+         1000:{
+             items:1
+         }
+     }
+ })
+
+// testimonial
+ $('#testimonial').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
